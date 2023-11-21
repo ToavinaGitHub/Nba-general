e8da171c1881181c517fe4d6e52391004cfdd630
Zak <118508607+ToavinaGitHub@users.noreply.github.com>
2023-11-21 13:10:58 +0600

    Create-ws-project

---
+++ Nba-ws/src/main/java/com/nba/nbaws/NbaWsApplication.java
@@ -0,0 +1,13 @@
+package com.nba.nbaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaWsApplication.class, args);
+	}
+
+}
---
+++ Nba-ws/src/test/java/com/nba/nbaws/NbaWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nba.nbaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
