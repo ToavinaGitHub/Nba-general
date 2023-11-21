61ad163293d772e73b69aa17d8e7d16067be6864
Toky <119029107+zenko03@users.noreply.github.com>
2023-11-21 18:18:46 +0100

    n

--- Nba-apk/capacitor.config.ts
+++ Nba-apk/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.nbaapk',
   appName: 'nbaApk',
-  webDir: 'build',
+  webDir: 'dist',
   server: {
     androidScheme: 'https'
   }
